bc0b7f7aaaa9451f0587e8c43dd4f80ed7486b16
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-11 13:39:59 -0300

    Criando função hash

---
+++ Hash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+
+    internal class Hash
+    {
+        internal class Element
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+
+            public Element(object value)
+            {
+                Key = this.Key;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return $"{this.Key}: {this.Value}";
+            }
+        }
+
+        public int Size{ get; set; }
+        public Element[] Elements{ get; set; }
+        public Hash(int size)
+        {
+            this.Size = size;
+            this.Elements = new Element[size];
+        }
+
+        public int HashFunc(object key)
+        {
+            return (this.Size % key.GetHashCode());
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Hash.Hash hash = new(7);
+
+Hash.Element element1 = new(5);
+Console.WriteLine(hash.HashFunc(element1));
