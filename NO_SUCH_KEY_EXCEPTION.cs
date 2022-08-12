de207e78a7040a3f9c98431e5435c2ea19577b85
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-12 13:58:23 -0300

    Adicionando exceção NO_SUCH_KEY

---
+++ NO_SUCH_KEY_EXCEPTION.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    internal class NO_SUCH_KEY_EXCEPTION : Exception
+    {
+        public NO_SUCH_KEY_EXCEPTION(string message) : base(message)    
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
