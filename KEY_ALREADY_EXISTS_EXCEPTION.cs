e74a2c72465e49452a2d3d1cb041e0943d331cf0
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-12 14:58:37 -0300

    Adicionando exceção KEY_ALREADY_EXISTS_EXCEPTION

---
+++ KEY_ALREADY_EXISTS_EXCEPTION.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    internal class KEY_ALREADY_EXISTS_EXCEPTION : Exception
+    {
+        public KEY_ALREADY_EXISTS_EXCEPTION(string message) : base(message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
